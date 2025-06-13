0d608179a4901e81b2d89c76c18441bf361b8bcf
kumanan <kuamanankmk17gmail.com>
2025-06-13 11:43:53 +0100

    python code has been added

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("hello world")
+name =" kumanan"
+age = "20"
+print("my name is " + name)
+print("my age is " + age)   
+
+
+
+#import keyword
+
+#print(keyword.kwlist)
+
+text = ''' this is a multi-line string
+
+it can span multiple lines.'''
+print(text)
+
+print ("hi my name is {} and my age is {}".format(name, age))
