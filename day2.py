d225c19e331593f5438ac68117746d3a521970a2
kumanan <kuamanankmk17gmail.com>
2025-06-14 11:18:13 +0100

    day2 file added

---
+++ day2.py
@@ -0,0 +1,39 @@
+#a="cfh"
+#a=24
+#a=23.321
+#a= 2+ 52j
+#print(type(a))
+
+#a = 12
+#b = 5
+#print(a+b)
+#print(a-b)
+#print(a*b)      
+#print(a/b)
+#print(a//b)    
+#print(a%b)
+#print (type(a**b))    
+
+
+
+import math
+
+#a=32
+#a = 16
+#print(math.sqrt(a))
+#print (math.pow(15,2))
+#print (math.floor(15.9))
+#print (math.ceil(15.1))
+ 
+
+
+
+import random
+
+#print (random.random()) 
+#print (random.randint(1, 100))  
+#print (random.choice(['apple', 'banana', 'cherry']  ))
+
+
+FILE_SIZE=1_00_000
+print(FILE_SIZE)
