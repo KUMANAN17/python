0c08305d854eff72bbd6c0dc713e147d683b6581
kumanan <kuamanankmk17gmail.com>
2025-06-16 11:43:16 +0100

    day3 python code added

---
+++ day3.py
@@ -0,0 +1,68 @@
+#age = 20
+#if age >= 18:
+ #   print("You are an adult.")
+#else:
+    #print("You are a minor.")
+
+
+#a = 33
+#b = 33
+#if b > a:
+  #print("b is greater than a")
+#elif a == b:
+  #print("a and b are equal")
+
+
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+
+# n = -355
+
+# res = "Positive" if n > 0 else "Negative" if n < 0 else "Zero"
+
+# print(res)
+
+# n = 99
+# for i in range(0, 50, 5 ):
+#     print(i)
+
+
+
+
+# max =8
+# counter = 0
+# while counter < max:
+#     print("Counter is:", counter)
+#     counter += 1
+
+# max = 42
+# counter = 0
+# while counter < max:
+#     print("Counter is:", counter)
+#     counter += 1
+
+
+
+# max = 55
+# counter = 45
+# while counter < max:
+#     print("Counter is:", counter)
+#     counter += 1
+
+
+
+number = int(input('Enter a number: '))
+
+if number > 0:
+    print('Positive number')
+else:
+    print('Not a positive number')
