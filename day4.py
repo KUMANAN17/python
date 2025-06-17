c2ce56651427a6b997c79e68b58dc8c4829dba60
kumanan <kuamanankmk17gmail.com>
2025-06-17 11:46:50 +0100

    python day4

---
+++ day4.py
@@ -0,0 +1,78 @@
+# def fun():
+#     print("Welcome to GFG")
+# fun()
+
+
+# def evenOdd(x):
+#     if (x % 2 == 0):
+#         print("even")
+#     else:
+#         print("odd")
+# evenOdd(2)
+# evenOdd(3)
+
+
+# def myFun(x, y=50):
+#     print("x: ", x)
+#     print("y: ", y)
+# myFun(10)
+
+
+# def greet(name, msg="Good morning!"):
+#     print("Hello", name + ', ' + msg)
+# greet("Alice")
+# greet("Bob", "How do you do?")      
+
+
+# def greet (name):
+#     return "Hello  {name}"
+# print(greet("Alice"))
+# print(greet("Bob"))
+# print(greet("Charlie"))
+
+
+# def greet (name):
+#     return f"Hello  {name}"
+# print(greet("Alice"))
+# print(greet("Bob"))
+# print(greet("Charlie"))
+
+# def ctof(c):
+#     return (c * 9/5) + 32
+# print(ctof(23))  
+# print(ctof(52)) 
+# print(ctof(26))
+# print(ctof(41))
+# print(ctof(37))
+# print(ctof(10))     
+# print(ctof(50))
+# print(ctof(75))
+
+
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#     return wrapper
+# @my_decorator
+# def say_hello():
+#     print("Hello!") 
+# say_hello()
+
+
+
+
+# Exception Handling
+
+# try:
+#     num = int(input("Enter a number: "))
+#     result = 100 / num  
+#     print(result)
+# except Exception as e:
+#     print( e)
+
+
+
+#  
