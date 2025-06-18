583d8cc26f2fb95e7f162623ad992cb9927a276c
kumanan <kuamanankmk17gmail.com>
2025-06-18 12:21:26 +0100

    day5 file

---
+++ day5.py
@@ -0,0 +1,24 @@
+# import pricing
+# final_price = pricing.get_final_price (100)
+# print (final_price)
+# print(pricing.say_hello())
+
+# import pricing as p
+# final_price = p.get_final_price (100)
+# print (final_price)
+# print(p.say_hello())
+
+# from pricing import say_hello , get_final_price
+# print(say_hello())
+# final_price = get_final_price(200)
+# print(final_price)
+
+# from store.pricing import get_final_price
+# print(get_final_price(300))
+
+
+
+
+import pandas as pd
+
+df= pd.DataFrame(['a','b','c','d'])
